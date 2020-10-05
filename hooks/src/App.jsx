79e72ac1fef36f5c8d45c02dd6acc63b05bfa1ae
expert99 <rrricardo00@yahoo.com.br>
2020-10-05 00:58:33 -0300

    aula

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import ButtonModal from './ButtonModal';
+import Modal from './Modal';
 
 const App = () => {
+
+    const [modal, setModal] = React.useState(false)
 
     const [ativo, setAtivo] = React.useState(true);
 
     return (
         <div>
-           <button onClick={() => setAtivo(!ativo)}>{ativo ? 'Ativado' : 'Desativado'}</button>
+            <div>{modal ? 'Modal aberto' : 'Modal fechado'} </div>
+            <Modal modal={modal} setModal={setModal}></Modal>
+            <ButtonModal setModal={setModal}></ButtonModal>
+           {/* <button onClick={() => setAtivo(!ativo)}>{ativo ? 'Ativado' : 'Desativado'}</button> */}
         </div>
     )
 }
---
+++ hooks/src/ButtonModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ButtonModal = ({setModal}) => {
+    
+    return (
+        <div>
+            <button onClick={() => setModal(true)}>Abrir</button>
+        </div> 
+    )
+}
+
+export default ButtonModal
