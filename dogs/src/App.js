7f12fb6b017003addf17f832a59418ba0f9ba391
expert99 <rrricardo00@yahoo.com.br>
2020-10-28 02:15:29 -0300

    aula

---
+++ dogs/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      testenpm 
+    </div>
+  );
+}
+
+export default App;
--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -4,15 +4,22 @@ import Modal from './Modal';
 
 const App = () => {
 
-    const [modal, setModal] = React.useState(false)
-
+    const [modal, setModal] = React.useState(false);
     const [ativo, setAtivo] = React.useState(true);
+
+    const [lista, setLista] = React.useState([]);
+
+    function handleClick(){
+        setLista([...lista, 'item']);
+    }
 
     return (
         <div>
-            <div>{modal ? 'Modal aberto' : 'Modal fechado'} </div>
+            {lista.map((item) => <li key={item}>{item}</li>)}
+            <button onClick={handleClick}>Adicionar</button>
+            {/* <div>{modal ? 'Modal aberto' : 'Modal fechado'} </div>
             <Modal modal={modal} setModal={setModal}></Modal>
-            <ButtonModal setModal={setModal}></ButtonModal>
+            <ButtonModal setModal={setModal}></ButtonModal> */}
            {/* <button onClick={() => setAtivo(!ativo)}>{ativo ? 'Ativado' : 'Desativado'}</button> */}
         </div>
     )
