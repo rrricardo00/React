3a6d011506f45955cbef33aaeedd1e478f6a0bb3
expert99 <rrricardo00@yahoo.com.br>
2020-10-28 02:15:06 -0300

    aula

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Produto from './Produto'
+
+export const App = () => {
+  return (
+    <div>
+     <Produto></Produto>
+    </div>
+  )
+}
+
+export default App;
---
+++ my-app/src/Produto.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Produto = () => {
+    return (
+        <div>
+            aqui estão os produtos
+        </div>
+    )
+}
+
+export default Produto
