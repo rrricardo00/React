66a6aa77a19f0cc9fa0e725a2ff0e84bd6072fc7
rrricardo00 <rrricardo00@yahoo.com.br>
2020-11-16 02:35:56 -0300

    aula

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+
+
+  const [contar, setContar] = React.useState(0);
+
+  React.useEffect(() =>{
+    console.log('executou effect')
+  }, []);
+
+  React.useEffect(() =>{
+    document.title = 'Total ' + contar;
+  }, [contar]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
