8a632a2d4a0441152545e0046aa3881f4c83027e
rrricardo00 <rrricardo00@yahoo.com.br>
2022-03-26 01:28:21 -0300

    react

--- meuapp/src/Produtos.jsx
+++ meuapp/src/Produtos.jsx
@@ -7,16 +7,16 @@ const Produtos = () => {
         { nome: 'Notebook', propriedades: ['16gb ram', '512gb'] },
         { nome: 'Smartphone', propriedades: ['2gb ram', '128gb'] },
     ];
-    
-    produtos.map(prod =>{
-        const {nome, propriedades} = prod;
+
+    produtos.map(prod => {
+        const { nome, propriedades } = prod;
         console.log(nome, propriedades)
     })
-    
+
     return (
         <section>
             <Titulo texto="Produtos"></Titulo>
-    {produtos.map(produtos => <Produtos2 key={produtos.nome} {...produtos}></Produtos2>)}
+            {produtos.map(produtos => <Produtos2 key={produtos.nome} {...produtos}></Produtos2>)}
         </section>
     )
 }
